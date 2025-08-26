b7a9088ab910189b2471fda7d137d1c6541a8079
Natan Siqueira Silva <natanssilva74@gmail.com>
2025-08-26 01:26:57 +0000

    estudo de caso das academias

---
+++ lista_prof.py
@@ -0,0 +1,18 @@
+alunos = [
+    {
+        "nome": "Ana",
+        "idade": 28,
+        "altura": 1.65,
+        "ativo": True,
+        "avaliacoes": [8.5, 9.0, 8.2]
+    },
+    {
+        "nome": "Carlos",
+        "idade": 22,
+        "altura": 1.80,
+        "ativo": False,
+        "avaliacoes": [7.0, 7.5, 8.0]
+    }
+]
+
+print(alunos)
